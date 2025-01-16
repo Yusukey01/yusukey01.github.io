b30c8a31716e747678a69ea8098de6c3d1addb31
Yusukey01 <luxor.1922.yy@ooutlook.com>
2025-01-16 09:11:31 -0800

    updated code  for II-8

--- Mathematics/Calculus/kkt_vs_pterms.py
+++ Mathematics/Calculus/kkt_vs_pterms.py
@@ -72,7 +72,7 @@ def penalty_method(rho=0.01, lr=0.05, tol=1e-6, max_iter=2000, rho_growth=1.2, c
 # KKT method
 def kkt_method(tol = 1e-8, max_iter = 10):
     # Initial values of parameters: x1, x2, x3, mu1, mu2, s1, s2, lambda
-    # Note: s1 and s2 are slack variables.
+    # Note: s1 and s2 are slack variables. 
     parameters = np.array([2.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.5]) 
 
     for i in range(max_iter):
