0e6ffe9f86c143fd2b1bf273981c45856538211e
Yusukey01 <luxor.1922.yy@ooutlook.com>
2025-03-31 17:43:57 -0700

    try new style

--- Mathematics/collapsible.js
+++ Mathematics/collapsible.js
@@ -2,7 +2,12 @@ const collapsibleBtns = document.querySelectorAll('.collapsible-btn');
 
 collapsibleBtns.forEach(btn => {
     btn.addEventListener('click', function() {
-        const content = this.nextElementSibling;
-        content.classList.toggle('active');
+        const content = this.nextElementSibling; // The <div> with the code
+        if (content.style.display === 'block') {
+            content.style.display = 'none'; // Hide the content
+    } else {
+            content.style.display = 'block'; // Show the content
+            }
     });
 });
+
