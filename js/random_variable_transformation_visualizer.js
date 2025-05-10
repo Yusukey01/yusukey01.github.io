14557426a9a2574b694e8955e4f36bda9c61fa19
Yusukey01 <luxor.1922.yy@ooutlook.com>
2025-05-09 20:02:14 -0700

    fix bimodal case in demo

--- js/random_variable_transformation_visualizer.js
+++ js/random_variable_transformation_visualizer.js
@@ -526,7 +526,7 @@ document.addEventListener('DOMContentLoaded', function() {
         errorLabel.textContent = 'Min HPD Density:';
       }
 
-      drawCanvas();
+      //drawCanvas();
       return;
 
     } else {
